94ecbe85a432379b9a80607b0d0a7044c52963c9
Washira <us_pou@hotmail.com>
2022-05-27 15:17:45 +0700

    Update router

--- routes/custom-tm-config-router.js
+++ routes/custom-tm-config-router.js
@@ -2,22 +2,22 @@ const router = require('express').Router();
 const queryToMongoose = require('../core/query-to-mongoose');
 const customTMConfigController = require('../controllers/custom-tm-config-controller');
 
-// GET localhost:3000/custom-rama/api/v1/productGroups
-router.get('/custom-rama/api/v1/customTMConfigs', queryToMongoose, customTMConfigController.getCustomTMConfigs);
+// GET localhost:3000/custom-tcels/api/v1/productGroups
+router.get('/custom-tcels/api/v1/customTMConfigs', queryToMongoose, customTMConfigController.getCustomTMConfigs);
 
-// GET localhost:3000/custom-rama/api/v1/productGroups/5dfce4d99140310364fcc0d9
-router.get('/custom-rama/api/v1/customTMConfigs/:customTMConfigId', customTMConfigController.getCustomTMConfig);
+// GET localhost:3000/custom-tcels/api/v1/productGroups/5dfce4d99140310364fcc0d9
+router.get('/custom-tcels/api/v1/customTMConfigs/:customTMConfigId', customTMConfigController.getCustomTMConfig);
 
-// PUT localhost:3000/custom-rama/api/v1/productGroups/5dfce4d99140310364fcc0d9
-router.put('/custom-rama/api/v1/customTMConfigs/:customTMConfigId', customTMConfigController.putCustomTMConfig);
+// PUT localhost:3000/custom-tcels/api/v1/productGroups/5dfce4d99140310364fcc0d9
+router.put('/custom-tcels/api/v1/customTMConfigs/:customTMConfigId', customTMConfigController.putCustomTMConfig);
 
-// DELETE localhost:3000/custom-rama/api/v1/productGroups/5dfce4d99140310364fcc0d9
-router.delete('/custom-rama/api/v1/customTMConfigs/:customTMConfigId', customTMConfigController.deleteCustomTMConfig);
+// DELETE localhost:3000/custom-tcels/api/v1/productGroups/5dfce4d99140310364fcc0d9
+router.delete('/custom-tcels/api/v1/customTMConfigs/:customTMConfigId', customTMConfigController.deleteCustomTMConfig);
 
-// POST localhost:3000/custom-rama/api/v1/customTMConfig/upload/productGroups/import-csv
-router.post('/custom-rama/api/v1/customTMConfig/upload/import-csv', customTMConfigController.multerImportCsv.single('file'), customTMConfigController.postImportCsv);
+// POST localhost:3000/custom-tcels/api/v1/customTMConfig/upload/productGroups/import-csv
+router.post('/custom-tcels/api/v1/customTMConfig/upload/import-csv', customTMConfigController.multerImportCsv.single('file'), customTMConfigController.postImportCsv);
 
-// POST localhost:3000/custom-rama/api/v1/productGroups
-router.post('/custom-rama/api/v1/customTMConfigs', customTMConfigController.postCustomTMConfig);
+// POST localhost:3000/custom-tcels/api/v1/productGroups
+router.post('/custom-tcels/api/v1/customTMConfigs', customTMConfigController.postCustomTMConfig);
 
 module.exports = router;
